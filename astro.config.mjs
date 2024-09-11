c2ec3cc5351f456cfa6f5c773478ed7e46cb0292
Reinny Almonte Ramos <reinny.almonte.ramos@gmail.com>
2024-09-12 00:12:27 +0100

    feat: using vercel analytics instead of google

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from 'astro/config';
 import { loadEnv } from "vite";
 import mdx from '@astrojs/mdx';
+import vercel from '@astrojs/vercel/serverless';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
 const { SITE_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
-// https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
   integrations: [mdx(), sitemap(), tailwind()]
 });
